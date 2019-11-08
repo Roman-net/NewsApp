3cdc054e86d01bbf614564a3169395258243fc42
Roman-net <57426099+roman-net@users.noreply.github.com>
2019-11-07 20:39:55 -0800

    Nov7draft

---
+++ WebApplication1Nov7draft/WebApplication1Nov7draft/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication1Nov7draft.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
