dc1841606e763f9717ea165da5c0487b42ab6c86
Roman-net <57426099+roman-net@users.noreply.github.com>
2019-12-02 19:52:20 -0800

    7pm
    7pm

--- CSD 412 Assignment 3/Controllers/HomeController.cs
+++ CSD 412 Assignment 3/Controllers/HomeController.cs
@@ -16,80 +16,47 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
 
-namespace CSD_412_Assignment_3.Controllers
-{
-
-    class Article
-    {
-
-        public string id { get; set; }
-        public string name { get; set; }
-        public string author { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        public string url { get; set; }
-        public int totalResults { get; set; }
-        public string urlToImage { get; set; }
-        public string publishedAt { get; set; }
-        public string content { get; set; }
-
-        public override string ToString()
-        {
-            string s = "";
-
-            s += $"Sourse id:{id}\nSourse name: {name}\narticle aouthor:{author}\narticle title: {title}\narticle description: {description}\narticle url: {url}\n published at {publishedAt}\ncontent: {content}";
-
-            return s;
-        }
-    }
-    class NewsAPIParse
-    {
-        public NewsAPIParse()
-        {
+namespace CSD_412_Assignment_3.Controllers {
+    
+    class NewsAPIParse{
+        public NewsAPIParse() {
 
         }
-        static void Parse()
-        {
+
+
+        public static List<NewsEntry> Parse() {
 
 
             Console.WriteLine("==================================================");
-
             string jsonText = "";
 
             WebClient client = new WebClient();
-            try
-            {
+            try {
                 Stream stream = client.OpenRead("https://newsapi.org/v2/top-headlines?country=us&apiKey=1085fdb5600240acbd2b45cc9d32361d");
-                using (StreamReader reader = new StreamReader(stream))
-                {
+                using (StreamReader reader = new StreamReader(stream)) {
                     jsonText = reader.ReadToEnd();
                 }
             }
-            catch (IOException ex)
-            {
+            catch (IOException ex){
                 Console.WriteLine("A network error occurred.  " + ex.Message);
                 Console.WriteLine("Unable to continue.");
-                return;
             }
 
             JObject newsJO = JObject.Parse(jsonText);
             var articlesJA = newsJO["articles"].Children().ToList();
 
-            var articles = new List<Article>();
-            foreach (var item in articlesJA)
-            {
-                articles.Add(item.ToObject<Article>());
+            var articles = new List<NewsEntry>();
+            foreach (var item in articlesJA){
+                articles.Add(item.ToObject<NewsEntry>());
             }
 
-
-            foreach (Article article in articles)
-            {
+            foreach (NewsEntry article in articles){
 
                 Console.WriteLine(article);
             }
 
-
-            Console.ReadLine();
+            return articles;
+            //Console.ReadLine();
         }
     }
     public class HomeController : Controller
@@ -101,58 +68,24 @@ namespace CSD_412_Assignment_3.Controllers
             _logger = logger;
         }
 
-        /*
-        private List<NewsEntry> parseJson(string json)
-       // private string parseJson(string json)
-        {
-
-            string startPattern = "^[\\w\\W]+:\\[";
-            string endPattern = "\\]\\}$";
-
-            json = Regex.Replace(json, startPattern, "");
-            json = Regex.Replace(json, endPattern, "");
-            json = Regex.Replace(json, "},{", "},,{");
-           // return json;
-            
-            string[] articles = Regex.Split(json, "},,{");
-            List<NewsEntry> NewsEntries = new List<NewsEntry>();
-            foreach(string s in articles)
-            { NewsEntries.Add(JsonSerializer.Deserialize<NewsEntry>(s)); }
-
-            return NewsEntries;
-        }
-        */
+       // NewsAPIParse parse1 = new NewsAPIParse();
 
 
 
+        public IActionResult Index(){
 
-        NewsAPIParse parse1 = new NewsAPIParse();
-
-
-
-        public IActionResult Index()
-        {
-            // List<NewsEntry>
-
-
+            List<NewsEntry> articles  = NewsAPIParse.Parse();
             var url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=1085fdb5600240acbd2b45cc9d32361d";
-            //var json = new WebClient().DownloadString(url);
-            var json = parse1;
-
-            //  Console.WriteLine(json);
-
-            return View(parse1);
-            // return new ContentResult { Content = parseJson(json) };
+          
+            return View(articles);
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy() {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error(){
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
--- CSD 412 Assignment 3/Models/NewsEntry.cs
+++ CSD 412 Assignment 3/Models/NewsEntry.cs
@@ -5,27 +5,25 @@ using System.Threading.Tasks;
 
 namespace CSD_412_Assignment_3.Models
 {
+    public class NewsEntry
 
-    public class Source
     {
         public string id { get; set; }
         public string name { get; set; }
-
-    }
-
-    public class NewsEntry {
-        public Source source { get; set; }
         public string author { get; set; }
         public string title { get; set; }
         public string description { get; set; }
         public string url { get; set; }
+        public int totalResults { get; set; }
         public string urlToImage { get; set; }
         public string publishedAt { get; set; }
         public string content { get; set; }
 
-
-
-
-
+        public override string ToString()
+        {
+            string s = "";
+            s += $"Sourse id:{id}\nSourse name: {name}\narticle aouthor:{author}\narticle title: {title}\narticle description: {description}\narticle url: {url}\n published at {publishedAt}\ncontent: {content}";
+            return s;
+        }
     }
 }
